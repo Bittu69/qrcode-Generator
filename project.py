90494e54deb526d3d2ff3716f3b1fce3c34960fa
Hitanshu <hitanshugupta212@gmail.com>
2021-11-15 02:54:43 +0530

    Remove mode parameter

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import pyqrcode
 data = input("Enter the text or link to generate QR code: ")
 
 # Using pyqrcode.create() to create a qr code of the input data
-qr = pyqrcode.create(data, error='H', version=30, mode='binary')
+qr = pyqrcode.create(data, error='H', version=30)
 
-qr.png('qr_code.png', scale=10, module_color=(0, 0, 0, 255),
+qr.png('qr_code.png', scale=1, module_color=(0, 0, 0, 255),
        background=(0xff, 0xff, 0xff, 0x88), quiet_zone=4)
