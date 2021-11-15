d627d3a36bfd0a98c5589f758aa93fde3149170a
Hitanshu <hitanshugupta212@gmail.com>
2021-11-15 14:33:52 +0530

    Change some parameters in pyqrcode.create() function

--- project.py
+++ project.py
@@ -1,11 +1,13 @@
 # Importing library
 import pyqrcode
-"""First We make 'data' as a variable and then to get some user input to create a QR Code."""
 
+"""First We make 'data' as a variable and then to get some user input to create a QR Code."""
 data = input("Enter the text or link to generate QR code: ")
 
-# Using pyqrcode.create() to create a qr code of the input data
-qr = pyqrcode.create(data, error='H', version=30)
 
-qr.png('qr_code.png', scale=1, module_color=(0, 0, 0, 255),
-       background=(0xff, 0xff, 0xff, 0x88), quiet_zone=4)
+# Using pyqrcode.create() to create a qr code of the input data
+qr = pyqrcode.create(data)
+
+# Here we use qr.png() to save the image of qrcode.
+qr.png('qr_code.png', scale=10, module_color=(0, 0, 0, 255),
+       background=(255, 255, 255, 255), quiet_zone=4)
